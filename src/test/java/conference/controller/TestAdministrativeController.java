c76becb7ab613abe6ae341c1d36d01647dea66f4
PupsTrash <891269@mail.ru>
2021-08-19 02:27:38 +0500

    fix test

--- src/test/java/conference/controller/TestAdministrativeController.java
+++ src/test/java/conference/controller/TestAdministrativeController.java
@@ -1,9 +1,12 @@
 package conference.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import conference.controller.api.RegistrationRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,7 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
 public class TestAdministrativeController {
     @Autowired
     MockMvc mockMvc;
@@ -25,11 +27,17 @@ public class TestAdministrativeController {
     @Autowired
     AdministrativeController controller;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     @WithAnonymousUser
-    public void testAccess() throws Exception{
-        this.mockMvc.perform(post("/api/v1/registration/register").content("{\"username\":\"testcase\"," +
-                        "\"password\":\"1\"}").contentType("application/json"))
+    public void testAccess() throws Exception {
+
+        var dto = new RegistrationRequestDto("testcase", "1");
+        var request = objectMapper.writeValueAsString(dto);
+
+        this.mockMvc.perform(post("/api/v1/registration/register").content(request).contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
